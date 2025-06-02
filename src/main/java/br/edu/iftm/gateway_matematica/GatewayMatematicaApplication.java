d32bd806d38b2e010a0ef3e27e34f073ef18ac57
fromanoel <fernanda.romanoel@estudante.iftm.edu.br>
2025-06-01 23:13:46 -0300

    first commit

---
+++ src/main/java/br/edu/iftm/gateway_matematica/GatewayMatematicaApplication.java
@@ -0,0 +1,12 @@
+package br.edu.iftm.gateway_matematica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+public class GatewayMatematicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayMatematicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iftm/gateway_matematica/GatewayMatematicaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iftm.gateway_matematica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayMatematicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
